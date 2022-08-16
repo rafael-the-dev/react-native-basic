824c78cb18937b26dc1327c2c1f9b11865c384b7
Rafael Tivane <luislangabusiness@gmail.com>
2022-08-16 14:52:13 +0200

    create event handlers to increase/decrease number

--- App.js
+++ App.js
@@ -2,16 +2,23 @@ import { useCallback, useState } from "react"
 import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    const [ number, setNumber ] = useState(0)
+    const [ number, setNumber ] = useState(0);
+
+    const increaseHandler = useCallback(() => setNumber(currentNumber => {
+        console.log(currentNumber); return currentNumber + 1;
+    }), []);
+    const decreaseHandler = useCallback(() => setNumber(currentNumber => currentNumber - 1), []);
 
     return (
         <View style={styles.container}>
             <Text style={styles.text}>{ number }</Text>
             <View style={styles.buttonsContainer}>
                 <Button 
+                    onPress={increaseHandler}
                     title="+"
                 />
                 <Button 
+                    onPress={decreaseHandler}
                     title="-"
                 />
             </View>
