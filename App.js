019ba095dc5f42347d75c5d2eaaa7c6e32792a27
Rafael Tivane <luislangabusiness@gmail.com>
2022-08-16 15:27:44 +0200

    learning about ImageBackground component

--- App.js
+++ App.js
@@ -1,47 +1,34 @@
 import { useCallback, useState } from "react"
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, TextInput, View } from 'react-native';
+
+const image = { uri: "assets/image-header.jpg" };
 
 export default function App() {
-    const [ value, setValue ] = useState("");
-
-    const onChangeHandler = useCallback((text) => {
-        setValue(text);
-    }, [])
+    
 
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>{ value }</Text>
-            <View style={styles.inputContainer}>
-                <TextInput 
-                    onChangeText={onChangeHandler}
-                    placeholder="Insert text here"
-                    style={styles.input}
-                    value={value}
-                />
-            </View>
+            <ImageBackground
+                resizeMode="cover"
+                source={require("./assets/image-header.jpg")}
+                style={styles.imageBackground}>
+                <Text style={styles.text}>Some random text</Text>
+            </ImageBackground>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  imageBackground: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center"
   },
   text: {
-    fontSize: '1.2rem',
-    fontWeight: "bold"
-  },
-  inputContainer: {
-    marginTop: '1.4rem'
-  },
-  input: {
-    borderColor: "#000",
-    borderStyle: "solid",
-    borderWidth: 1,
-    height: 40,
-    padding: 10
+    color: "#FFF",
+    fontSize: '2rem'
   }
 });
