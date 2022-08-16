b8a4b348b5e04d24a1c66c41478612ed60df15d9
Rafael Tivane <luislangabusiness@gmail.com>
2022-08-16 18:33:53 +0200

    show loading message

--- App.js
+++ App.js
@@ -5,17 +5,34 @@ import Card from "./components/country-card";
 
 export default function App() {
     const [ data, setData ] = useState([]);
+    const [ loading, setLoading ] = useState(false);
 
     const fetchData = useCallback(() => {
+        setLoading(true);
+
         fetch("https://restcountries.com/v3.1/all")
             .then(res => res.json())
-            .then(countries => setData(countries))
-            .catch(console.error)
+            .then(countries => {
+                setLoading(false);
+                setData(countries);
+            })
+            .catch(error => {
+                setLoading(false);
+                console.error(error);
+            })
     }, []);
 
     useEffect(() => {
         fetchData();
     }, [ fetchData ])
+
+    if(loading) {
+        return (
+            <View style={styles.loadingContainer}>
+                <Text style={styles.loadingText}>Loading...</Text>
+            </View>
+        )
+    }
     
     return (
         <View style={styles.container}>
@@ -28,16 +45,25 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  imageBackground: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  text: {
-    color: "#FFF",
-    fontSize: '2rem'
-  }
+    loadingContainer: {
+        alignItems: "center",
+        flex: 1,
+        justifyContent: "center"
+    },
+    loadingText: {
+        fontSize: "1.3rem"
+    },
+    container: {
+        flex: 1,
+        padding: "32rem 5% 0 5%"
+    },
+    imageBackground: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    text: {
+        color: "#FFF",
+        fontSize: '2rem'
+    }
 });
