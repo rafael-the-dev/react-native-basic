d6ac7ab767ca7f89f0d1eecff078c9ee85146995
Rafael Tivane <luislangabusiness@gmail.com>
2022-08-17 17:22:45 +0200

    test Array map method and key property

--- App.js
+++ App.js
@@ -1,52 +1,35 @@
-import { useCallback, useEffect, useState } from "react"
-import { FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useCallback, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-import { useFetch } from "./hooks/useFetch"
+const App = () => {
+    const [ people, setPeople ] = useState([
+        { key: "1", name: "Rafael Tivane" },
+        { key: "2", name: "Lopes Rafael" },
+        { key: "3", name: "Egness Da Flo" },
+        { key: "4", name: "Hilario Vicente" },
+        { key: "5", name: "Arnaldo Manecusse" }
+    ]);
 
-import Card from "./components/country-card";
-
-export default function App() {
-    
-    const { data, loading } = useFetch({ url: "https://restcountries.com/v3.1/all" });
-
-    if(loading) {
-        return (
-            <View style={styles.loadingContainer}>
-                <Text style={styles.loadingText}>Loading...</Text>
-            </View>
-        )
-    }
-    
     return (
         <View style={styles.container}>
-            <FlatList 
-                data={data}
-                renderItem={({ item }) => <Card { ...item } />}
-            />
+             {
+                people.map(person => (
+                    <View key={person.key}>
+                        <Text>{ person.name }</Text>
+                    </View>
+                ))
+             }
         </View>
     );
-}
+};
 
 const styles = StyleSheet.create({
-    loadingContainer: {
-        alignItems: "center",
-        flex: 1,
-        justifyContent: "center"
-    },
-    loadingText: {
-        fontSize: "1.3rem"
-    },
     container: {
+        backgroundColor: "#FFF",
         flex: 1,
-        padding: "32rem 5% 0 5%"
-    },
-    imageBackground: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center"
-    },
-    text: {
-        color: "#FFF",
-        fontSize: '2rem'
+        paddingHorizontal: 10,
+        paddingTop: 20
     }
-});
+})
+
+export default App;
